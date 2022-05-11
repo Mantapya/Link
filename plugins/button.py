72723b0c83259b2974b01b7b58f808b9d2aea7d2
Mantapya <105369553+Mantapya@users.noreply.github.com>
2022-05-11 23:36:49 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardButton
 
 
 def start_button(client):
-    if not FORCE_SUB_CHANNEL,FORCE_SUB_CHANNEL1 and not FORCE_SUB_GROUP:
+    if not FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
         buttons = [
             [
                 InlineKeyboardButton(text="• 𝚃𝚎𝚗𝚝𝚊𝚗𝚐 𝙰𝚔𝚞 •", callback_data="about"),
